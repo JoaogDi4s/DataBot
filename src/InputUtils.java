01c6ee7a1e30a2d34dc461b09aa0bd97eabbec65
João Guilherme <164015979+JoaogDi4s@users.noreply.github.com>
2024-11-08 19:29:56 -0300

    Add files via upload

---
+++ src/InputUtils.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class InputUtils {
+    public static String getInput(Scanner scan) {
+        return scan.nextLine().toLowerCase().trim(); //PASSA PARA LOWERCASE E TIRA ESPAÇO
+    }
+}
